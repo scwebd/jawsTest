db2b27d67454c229e760c4b396a3cbe9f7b62bdf
scwebd <scwebd@gmail.com>
2019-04-15 11:02:49 -0600

    Connection change

--- config/connection.js
+++ config/connection.js
@@ -2,20 +2,26 @@
 var mysql = require("mysql");
 var connection;
 
-if(process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSBD_URL);
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSBD_URL);
 } else {
-  connection = mysql.createConnection ({
-  host: "localhost",
-  port: 8889,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 8889,
+        user: "root",
+        password: "root",
+        database: "burgers_db"
+    });
 };
 
 // Make connection.
-connection.connect();
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
 // Export connection for our ORM to use.
 module.exports = connection;
